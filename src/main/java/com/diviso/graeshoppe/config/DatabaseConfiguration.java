d179a85fc6c350a665bc2be4eb8ad28cf4753db1
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-09-19 13:52:12 +0530

    10 commit

--- src/main/java/com/diviso/graeshoppe/config/DatabaseConfiguration.java
+++ src/main/java/com/diviso/graeshoppe/config/DatabaseConfiguration.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+//import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories("com.diviso.graeshoppe.repository")
 @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
 @EnableTransactionManagement
-@EnableElasticsearchRepositories("com.diviso.graeshoppe.repository.search")
+//@EnableElasticsearchRepositories("com.diviso.graeshoppe.repository.search")
 public class DatabaseConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
