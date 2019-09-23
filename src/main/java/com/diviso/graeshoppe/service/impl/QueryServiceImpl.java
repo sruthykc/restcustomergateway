4f62c492980ac40974d6739c7421564675970571
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-09-23 12:12:13 +0530

    28 commit

--- src/main/java/com/diviso/graeshoppe/service/impl/QueryServiceImpl.java
+++ src/main/java/com/diviso/graeshoppe/service/impl/QueryServiceImpl.java
@@ -150,10 +150,10 @@ public class QueryServiceImpl implements QueryService {
 		SearchRequest searchRequest = new SearchRequest("product");
 		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
 		
-		String[] includeFields = new String[] {"product"};
+	/*	String[] includeFields = new String[] {"product"};
 	    String[] excludeFields = new String[] {"category"};
 	    searchSourceBuilder.fetchSource(includeFields, excludeFields);
-	  searchSourceBuilder.fetchSource(true);
+	  searchSourceBuilder.fetchSource(true);*/
 		searchSourceBuilder.query(matchAllQuery());
 		searchSourceBuilder.from(0);
 		searchSourceBuilder.size(10);
