7721a1f8a8e114c037c3ef8fa0ef8c3098af2b5c
sruthykc <sruthy.k.c@gmail.com>
2019-11-27 10:51:48 +0530

    swaggered new customer

--- src/main/java/com/diviso/graeshoppe/client/customer/api/ContactResourceApiClient.java
+++ src/main/java/com/diviso/graeshoppe/client/customer/api/ContactResourceApiClient.java
@@ -1,8 +1,8 @@
 package com.diviso.graeshoppe.client.customer.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import com.diviso.graeshoppe.client.customer.ClientConfiguration;
+import com.diviso.graeshoppe.client.customer.CustomerClientConfiguration;
 
-@FeignClient(name="${customer.name:customer}", url="${customer.url:35.238.35.19:8088/}", configuration = ClientConfiguration.class)
+@FeignClient(name="${customer.name:customer}", url="${customer.url:35.238.35.19:8088/}", configuration = CustomerClientConfiguration.class)
 public interface ContactResourceApiClient extends ContactResourceApi {
 }
