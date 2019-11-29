67da71f44ff992dea528a20d25f246bfa92e0069
Sruthy <sruthy.k.c@lxisoft.com>
2019-11-29 14:27:38 +0530

    add package

--- src/main/java/com/diviso/graeshoppe/web/rest/QueryResource.java
+++ src/main/java/com/diviso/graeshoppe/web/rest/QueryResource.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
