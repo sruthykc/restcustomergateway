3e9bf410e9e577d7e39416202537f1c6de5dac84
abilash <Abilash.s@lxisoft.com>
2019-11-30 16:13:40 +0530

    changed elastic configurations

--- src/main/java/com/diviso/graeshoppe/config/ElasticRestClientConfig.java
+++ src/main/java/com/diviso/graeshoppe/config/ElasticRestClientConfig.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ElasticRestClientConfig {
 	
-    @Value("${elasticsearch.host:039a8d60bba54e46a0021ba33cb2d15d.us-west1.gcp.cloud.es.io}")
+    @Value("${elasticsearch.host:20a60de2fab94b1b9aca60bcfee1591b.us-central1.gcp.cloud.es.io}")
     public String host;
     @Value("${elasticsearch.port:9243}")
     public int port;
@@ -31,7 +31,7 @@ public class ElasticRestClientConfig {
     public RestHighLevelClient client(){
         System.out.println("host:"+ host+"port:"+port);
         final CredentialsProvider credentialsProvider =new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY,new UsernamePasswordCredentials("elastic", "oG1Sm3v2HCjBJRvIVxnWtGCL"));
+        credentialsProvider.setCredentials(AuthScope.ANY,new UsernamePasswordCredentials("elastic", "7hcaOIiz0JiemFumZFRsLz6E"));
         RestClientBuilder builder =RestClient.builder(new HttpHost(host, port, "https")).setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
         builder.setRequestConfigCallback(requestConfigBuilder -> requestConfigBuilder.setConnectTimeout(timeout * 1000).setSocketTimeout(timeout * 1000)
                 .setConnectionRequestTimeout(0));
