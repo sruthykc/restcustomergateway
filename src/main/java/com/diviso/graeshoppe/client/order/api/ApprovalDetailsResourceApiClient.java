9d1ba259483f3487c2f31b0586c3d8362e0ee9ce
abilash <Abilash.s@lxisoft.com>
2019-12-06 15:50:51 +0530

    changed url

--- src/main/java/com/diviso/graeshoppe/client/order/api/ApprovalDetailsResourceApiClient.java
+++ src/main/java/com/diviso/graeshoppe/client/order/api/ApprovalDetailsResourceApiClient.java
@@ -3,6 +3,6 @@ package com.diviso.graeshoppe.client.order.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import com.diviso.graeshoppe.client.order.OrderClientConfiguration;
 
-@FeignClient(name="${order.name:order}", url="${order.url:dev.ci2.divisosofttech.com:8088/}", configuration = OrderClientConfiguration.class)
+@FeignClient(name="${order.name:order}", url="${order.url:35.238.35.19:8088/}", configuration = OrderClientConfiguration.class)
 public interface ApprovalDetailsResourceApiClient extends ApprovalDetailsResourceApi {
 }
